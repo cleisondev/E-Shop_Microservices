5c0ff83e98d30530bc14340a8e2fbf49d9e75db6
Cleison Nunes <cleison.nunes20@gmail.com>
2025-05-29 23:45:20 -0300

    implementing GetProducts endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -16,21 +16,30 @@ namespace Catalog.API.Products.CreateProduct
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            var product = new Product
+            try
             {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price,
-            };
+                var product = new Product
+                {
+                    Name = command.Name,
+                    Category = command.Category,
+                    Description = command.Description,
+                    ImageFile = command.ImageFile,
+                    Price = command.Price,
+                };
 
-            //TODO
-            //save database
-            session.Store(product);
-            await session.SaveChangesAsync(cancellationToken);
+                //TODO
+                //save database
+                session.Store(product);
+                await session.SaveChangesAsync(cancellationToken);
 
-            return new CreateProductResult(product.Id);
+                return new CreateProductResult(product.Id);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
         }
     };
 
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsRequest();
+    public record GetProductsResponse(IEnumerable<Product> products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public async void AddRoutes(IEndpointRouteBuilder app)
+        {
+             app.MapGet("/products", async (ISender sender) =>
+            {
+
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProduct")
+                .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product")
+                .WithDescription("Get Product");
+        }
+    }
+}
