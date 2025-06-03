dded4afc248e434a26f0d4c2cc29fb1c41dbd373
Cleison Nunes <cleison.nunes20@gmail.com>
2025-06-02 21:11:47 -0300

    Adicionado endpoint GetProductByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+    
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var products = session.Query<Product>()
+                .Where(p => p.Category.Contains(request.Category))
+                .ToList();
+
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -5,19 +5,13 @@ namespace Catalog.API.Products.GetProductById
 
     public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
     public record GetProductByIdResult(Product Product);
-    public class GetProductByIdHandler 
+    public class GetProductByIdHandler(IDocumentSession session) 
         : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
-        private readonly IDocumentSession _session;
-
-        public GetProductByIdHandler(IDocumentSession session)
-        {
-            _session = session;
-        }
 
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            var product = await _session.LoadAsync<Product>(query.Id, cancellationToken);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
             if (product is null)
                 throw new ProductNotFoundException();
