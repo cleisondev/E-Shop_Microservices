6d84403a9b0bdb5a94560ec908d96a92900179f5
Cleison Nunes <cleison.nunes20@gmail.com>
2025-06-02 21:47:49 -0300

    Adicionando update endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest
+        (Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<UpdateProductResult>;
+    public record UpdateProducResponse(Guid Id);
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProducResponse>();
+            })
+                .WithName("UpdateProduct")
+                .Produces<UpdateProducResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Update Product")
+                .WithDescription("Update Product");
+        }
+    }
+}
