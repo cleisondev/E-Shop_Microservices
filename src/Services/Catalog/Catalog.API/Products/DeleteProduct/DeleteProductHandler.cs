8f384f1ae0cf2d28505324051a7cee322b4a2d0a
Cleison Nunes <cleison.nunes20@gmail.com>
2025-06-02 22:29:17 -0300

    adicionando endpoint DeleteProduct

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id)
+        : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool Deleted);
+    public class DeleteProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var product = await session.LoadAsync<Product>(request.Id);
+
+                if (product is null)
+                    throw new ProductNotFoundException();
+
+                session.Delete(product);
+                await session.SaveChangesAsync(cancellationToken);
+
+                return new DeleteProductResult(true);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
