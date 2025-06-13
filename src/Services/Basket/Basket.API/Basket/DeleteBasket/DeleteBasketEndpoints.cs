e0653e7e7cf05ab3c23b996771e4665b3a5cdeff
Cleison Nunes <cleison.nunes20@gmail.com>
2025-06-12 22:01:47 -0300

    Adding DeleteBasket Endpoint

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.DeleteBasket
+{
+    //public record DeleteBasketRequest(string Username);
+    public record class DeleteBasketResponse(string Username);
+
+    public class DeleteBasketEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{username}", async (string username, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(username));
+                var response =  result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket
+{
+    public record DeleteBasketCommand(string Username) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(string Username);
+
+    public class DeleteBasketHandler(IDocumentSession session)
+        : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            var cart = session.Load<ShoppingCart>(request.Username);    
+            if(cart is null)
+                throw new Exception($"Basket with username {request.Username} not found.");
+
+            session.Delete(cart);
+
+            return session.SaveChangesAsync(cancellationToken)
+                .ContinueWith(_ => new DeleteBasketResult(request.Username), cancellationToken);
+        }
+    }
+}
