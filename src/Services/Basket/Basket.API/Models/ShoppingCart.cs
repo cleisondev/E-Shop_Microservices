9b5a61a337afedb2dc5c0c92bf53f1448834e444
Cleison Nunes <cleison.nunes20@gmail.com>
2025-06-12 21:51:10 -0300

    Creating Basket MS

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        public ShoppingCart(string userName)
+        {
+            Username = userName;
+        }
+
+        public ShoppingCart()
+        {
+                
+        }
+    }
+}
