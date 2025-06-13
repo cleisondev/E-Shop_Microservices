5897ce933b2e062913e1710067fd5f7abb108de5
Cleison Nunes <cleison.nunes20@gmail.com>
2025-06-12 21:51:24 -0300

    Addin GetBasket endpoint

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.GetBasket
+{
+    //public record GetBasketRequest(string Username);
+    public record GetBasketResponse(ShoppingCart Cart);
+
+    public class GetBasketEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{username}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(userName));
+                var response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetBasket")
+                .WithSummary("Get a user's shopping cart by username")
+                .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get a user's shopping cart by username")
+                .WithDescription("Basket")
+                ;
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketHandler(IDocumentSession session)
+        : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+        {
+            var cart = await session.LoadAsync<ShoppingCart>(request.Username, cancellationToken);
+
+            return new GetBasketResult(cart ?? new ShoppingCart
+            {
+                Username = request.Username,
+                Items = new List<ShoppingCartItem>()
+            });
+        }
+    }
+}
