9688a190deb94c91334f34f89b945f2e85a50042
cleisondev <cleison.dev@hotmail.com>
2025-08-20 20:10:37 -0300

    Feature: Implementing discount database

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,11 +1,15 @@
 
 
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database")!));
 
 var app = builder.Build();
 
@@ -14,3 +18,4 @@ app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+  
