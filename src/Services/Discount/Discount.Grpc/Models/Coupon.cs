3f7f69f1fc0bd2ed7600f552462573c6218e2b07
cleisondev <cleison.dev@hotmail.com>
2025-08-20 21:06:46 -0300

    Feature: seeding database

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -3,8 +3,8 @@
     public class Coupon
     {
         public int Id { get; set; }
-        public int ProductName { get; set; } = default;
-        public int Description { get; set; } = default;
+        public string ProductName { get; set; } = default;
+        public string Description { get; set; } = default;
         public int Amount { get; set; }
     }
 }
