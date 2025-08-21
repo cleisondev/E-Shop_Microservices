bcdf816e94611cdd82dd663a48a4b2f9615e10eb
cleisondev <cleison.dev@hotmail.com>
2025-08-20 21:32:04 -0300

    feature: implementing crud

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -2,6 +2,7 @@
 using Discount.Grpc.Models;
 using Grpc.Core;
 using Mapster;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
@@ -23,17 +24,47 @@ namespace Discount.Grpc.Services
             return couponModel;
         }
 
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request"));
+
+            dbContext.Coupons.Add(coupon);
+            await dbContext.SaveChangesAsync();
+
+            var couponModel = coupon.Adapt<CouponModel>();
+
+            return couponModel;
         }
-        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
         {
-            return base.UpdateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request"));
+
+            dbContext.Coupons.Update(coupon);
+            await  dbContext.SaveChangesAsync();
+
+            var couponModel = coupon.Adapt<CouponModel>();
+
+            return couponModel;
         }
-        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
         {
-            return base.DeleteDiscount(request, context);
+            var coupon = await dbContext.
+                Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+            if (coupon is null)
+               return new DeleteDiscountResponse { Success = false };
+
+            dbContext.Coupons.Remove(coupon);
+            var deleted = await dbContext.SaveChangesAsync() > 0;
+
+            return new DeleteDiscountResponse { Success = deleted };
+
         }
     }
 }
