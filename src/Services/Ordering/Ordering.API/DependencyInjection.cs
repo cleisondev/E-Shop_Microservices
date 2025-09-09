106a7dd9184103cfa87ff0a451755c7bc7c4f66f
cleisondev <cleison.dev@hotmail.com>
2025-09-08 21:32:58 -0300

    Adding projects ordering api

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            // Add API services here, e.g., Controllers, Swagger, CORS, etc.
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+                       // Configure the HTTP request pipeline here, e.g., Swagger, CORS, etc.
+            return app;
+        }
+    }
+}
