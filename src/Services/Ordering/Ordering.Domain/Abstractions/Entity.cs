e487beb7376b5eed0d98dda3540dfdeef2a65d35
cleisondev <cleison.dev@hotmail.com>
2025-09-15 15:35:27 -0300

    Adding DDD Abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public abstract T Id { get; set; }
+        public abstract DateTime? CreatedAt { get; set; }
+        public abstract string? CreatedBy { get; set; }
+        public abstract DateTime? LastModified { get; set; }
+        public abstract string? LastModifiedBy { get; set; }
+    }
+}
