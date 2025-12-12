c22deb6e80865559d29b50a2182803420ab65d78
cleisondev <cleison.dev@hotmail.com>
2025-12-11 22:34:08 -0300

    Adding DeleteHandler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommand(Guid orderId)
+        : ICommand<DeleteOrderResult>;
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.orderId).NotEmpty().WithMessage("OrderId is required");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext _dbContext)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            //Delete Order entity from command object
+            //save to database
+            //return result
+
+            var orderId = OrderId.Of(command.orderId);
+            var order = await _dbContext.Orders
+                .FindAsync([orderId], cancellationToken: cancellationToken);
+
+            if (order is null)
+            {
+                throw new OrderNotFoundException(command.orderId);
+            }
+
+            _dbContext.Orders.Remove(order);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
