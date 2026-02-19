9a720a2fadaee8c8554a5184ee8bc970ef33a116
cleisondev <cleison.dev@hotmail.com>
2026-02-18 21:11:35 -0300

    Feature: Adding GetOrderByName

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdateEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.EventHandlers
+{
+    internal class OrderUpdateEventHandler(ILogger<OrderUpdateEventHandler> logger)
+        : INotificationHandler<OrderUpdatedEvent>
+    {
+        public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling OrderUpdatedEvent for Order Id: {OrderId}", notification.order.Id);
+            //notification.order.Id.ToString();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameResult.cs
@@ -0,0 +1,6 @@
+﻿
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
