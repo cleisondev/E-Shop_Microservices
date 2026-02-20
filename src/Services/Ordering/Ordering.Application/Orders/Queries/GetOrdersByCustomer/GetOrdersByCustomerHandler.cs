066adec7a0af9dd498f2ddd422df993320feb893
cleisondev <cleison.dev@hotmail.com>
2026-02-19 21:29:36 -0300

    Fixing parameter name

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -1,6 +1,7 @@
 ﻿using BuildingBlocks.CQRS;
 using Microsoft.EntityFrameworkCore;
 using Ordering.Application.Data;
+using Ordering.Application.Extensions;
 using Ordering.Domain.ValueObjects;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
@@ -9,7 +10,7 @@ namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
     public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext)
         : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
     {
-        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery request, CancellationToken cancellationToken)
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
         {
             var orders = await dbContext.Orders
                       .Include(o => o.OrderItems)
