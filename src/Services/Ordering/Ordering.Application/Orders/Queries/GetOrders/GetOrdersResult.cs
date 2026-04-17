f7ea5f95b369afcc9bbb64ef7bb6349e66452920
cleisondev <cleison.dev@hotmail.com>
2026-04-16 21:42:58 -0300

    Implementing GetOrdersQuery

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersResult.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersResult(PaginatedResut<OrderDto> Orders);
+    
